4ac3ac8958d60283901bbfc01d24642cd6f1b6c7
arunsingh28 <arunsingh28aug.as@gmail.com>
2023-07-29 04:09:57 +0530

    implement debounced search

--- client/src/Components/Workspace.tsx
+++ client/src/Components/Workspace.tsx
@@ -17,17 +17,36 @@ const Workspace = () => {
 
     const [movieData, setMovieData] = useState<IMDB[] | null>(null)
 
+
+    // Define a function to fetch movies after a certain delay (e.g., 500ms)
+    const debounceFetchMovies = (searchText: string) => {
+        // Clear any previous timers
+        clearTimeout(300);
+
+        // Set a new timer to fetch movies after 500ms of user inactivity
+        const timer = setTimeout(() => {
+            fetchMovies(searchText)
+                .then((data) => {
+                    setMovieData(data.data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                    setMovieData([]); // Set movieData to an empty array in case of an error
+                });
+        }, 300);
+    };
+
+
     useEffect(() => {
-        fetchMovies(text).then((data) => {
-            setMovieData(data.data)
-            console.log(movieData)
-        }).catch((err) => {
-            console.log(err)
-        })
-    }, [text])
+        // Fetch movies using the debounced function
+        debounceFetchMovies(text);
+
+        // Cleanup function to clear any timers when the component unmounts
+        return () => clearTimeout(300);
+    }, [text]);
 
     return (
-        <div>
+        <div className='font-poppins'>
             {movieData === null ? ( // Show loading message when movieData is null
                 <div>Loading...</div>
             ) : movieData && movieData.length > 0 ? ( // Check if movieData is an array with data
